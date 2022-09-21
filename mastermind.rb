1c2fcf785cc662b814d341b9bdda66d79873a4ff
N Skutt <nskutt@protonmail.com>
2022-09-21 16:58:18 -0500

    add the tests for the guess

--- mastermind.rb
+++ mastermind.rb
@@ -7,6 +7,34 @@ class Codemaster
     set_code
   end
 
+  def check_code(guess)
+    victory if guess == @code
+    blackpegs(guess)
+  end
+
+  def blackpegs(guess)
+    @black = 0
+    i = 0
+    while i < @code.length
+      next unless @code[i] == guess[i]
+
+      @black += 1
+      guess[i] = 0
+    end
+    whitepegs(guess)
+  end
+
+  def whitepegs(guess)
+    @white = 0
+    i = 0
+    while i < @code.length
+      next unless @code.include?(guess[i])
+
+      @white += 1
+    end
+    p "Black: #{@black}, White: #{@white}"
+  end
+
   private
 
   def set_code
