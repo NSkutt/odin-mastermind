6c286c2779fcaeabd3513b8962ce443a6f4af303
N Skutt <nskutt@protonmail.com>
2022-09-22 18:27:13 -0500

    bug fixes, lots and lots of bug fixes

--- mastermind.rb
+++ mastermind.rb
@@ -2,37 +2,45 @@
 
 # Creates, contains, and checks against, the code
 class Codemaster
+  attr_reader :victory
+
   def initialize
     @choices = Array.new(4) { [1, 2, 3, 4, 5, 6] }
     set_code
   end
 
   def check_code(guess)
-    victory if guess == @code
+    p guess
+    return @victory = true if guess == @code
+
     blackpegs(guess)
   end
 
   def blackpegs(guess)
     @black = 0
     i = -1
-    while i < @code.length
+    arr = []
+    while i < (@code.length - 1)
       i += 1
+      arr.push(@code[i])
       next unless @code[i] == guess[i]
 
       @black += 1
       guess[i] = 0
+      arr[i] = 'x'
     end
-    whitepegs(guess)
+    whitepegs(guess, arr)
   end
 
-  def whitepegs(guess)
+  def whitepegs(guess, code)
     @white = 0
     i = -1
-    while i < @code.length
+    while i < (code.length - 1)
       i += 1
-      next unless @code.include?(guess[i])
+      next unless code.include?(guess[i])
 
       @white += 1
+      code.delete_at(code.index(guess[i]))
     end
     p "Black: #{@black}, White: #{@white}"
   end
@@ -41,6 +49,7 @@ class Codemaster
 
   def set_code
     @code = @choices.flatten.sample(4)
+    p @code
   end
 end
 
@@ -51,6 +60,7 @@ class Player
     @name = gets.chomp
     @error = false
     @codemaster = Codemaster.new
+    @count = 1
     input
   end
 
@@ -63,17 +73,36 @@ class Player
     @guess.delete_if { |guess| guess == ' ' }
     @guess.each_with_index { |item, idx| @guess[idx] = item.to_i }
     check_errors(@guess)
-    @codemaster.check_code(@guess)
+    play(@guess)
   end
 
   def check_errors(code)
     error unless code.is_a?(Array) && code.length == 4
     error unless code.each { |guess| guess < 7 && guess.positive? }
+    # code.each { |guess| p guess < 7  }
+    # code.each { |guess| p guess.positive? }
   end
 
   def error
     @error = true
     input
+  end
+
+  def play(code)
+    victory('loss') if @count > 10
+    @codemaster.check_code(code)
+    victory(@name) if @codemaster.victory == true
+    @count += 1
+    input
+  end
+
+  def victory(condition)
+    msg = condition == @name ? "Congratulations #{@name} you win!" : 'You lose, better luck next time'
+    p msg
+    p 'Would you like to play again?'
+    p 'Yes/No'
+    ans = gets.chomp
+    ans.upcase == 'YES' ? _again = Player.new : exit
   end
 end
 
